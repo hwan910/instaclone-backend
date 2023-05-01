07846d9fa8904f17389aa278674fb40fe12246bd
sunghwanPark <william@chicment.com>
2023-05-01 21:28:02 +0900

    Backend Setup

---
+++ schema.js
@@ -0,0 +1,14 @@
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+import { makeExecutableSchema } from "apollo-server";
+
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`)
+const typeDefs = mergeTypeDefs(loadedTypes);
+
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`)
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({typeDefs,resolvers})
+
+
+export default schema
