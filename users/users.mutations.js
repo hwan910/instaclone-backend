4d885bc22ae74ca2e8fe801d8dbe4139cfb5698e
sunghwanPark <william@chicment.com>
2023-05-01 22:11:48 +0900

    Create Account

---
+++ users/users.mutations.js
@@ -0,0 +1,25 @@
+import client from "../client";
+
+
+
+export default {
+  Mutation: {
+    createAccount: async(_, { firstName, lastName, userName, email, password }) => {
+      //check if username or email are already on DB
+      const existingUser = await client.user.findFirst({
+        where:{
+          OR: [
+            {
+              userName,
+            },
+            {
+              email
+            }
+          ]
+        }
+      });
+      // hash password
+      // save and return the user
+    },
+  },
+};
