22da10181b35fbe5326a9b77506df20f072f9f28
sunghwanPark <william@chicment.com>
2023-05-03 10:47:12 +0900

    try & catch

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,35 +7,41 @@ export default {
       _,
       { firstName, lastName, userName, email, password }
     ) => {
-      //check if username or email are already on DB
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              userName,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      const user = client.user.create({
-        data: {
-          userName,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
-      console.log(existingUser);
-      return user;
+      try {
+        //check if username or email are already on DB
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                userName,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        // hash password  1234 -> fn(1234) -> ad$f@s@3423# -> DB  => oneway Fn
+        // 니꼬 쌤의 해시함수 설명 영상: https://www.youtube.com/watch?v=67UwxR3ts2E
 
-      // hash password  1234 -> fn(1234) -> ad$f@s@3423# -> DB  => oneway Fn
-      // 니꼬 쌤의 해시함수 설명 영상: https://www.youtube.com/watch?v=67UwxR3ts2E
-      // save and return the user
+        const uglyPassword = await bcrypt.hash(password, 10);
+        if (existingUser) {
+          throw new Error("This username/password is already taken");
+        }
+        const user = client.user.create({
+          data: {
+            userName,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+        // save and return the user
+        return user;
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
