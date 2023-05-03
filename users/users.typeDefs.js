21fdccb80894772caecbf537be0c1fce5e9398ad
sunghwanPark <william@chicment.com>
2023-05-03 23:15:30 +0900

    login

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -10,6 +10,12 @@ export default gql`
     createdAt: String!
     updatedAt: String!
   }
+  type LoginResult {
+    ok: Boolean!
+    token: String
+    error: String
+  }
+
   type Mutation {
     createAccount(
       firstName: String!
@@ -18,7 +24,9 @@ export default gql`
       email: String!
       password: String!
     ): User
+    login(userName: String!, password: String!): LoginResult
   }
+
   type Query {
     seeProfile(userName: String): User
   }
