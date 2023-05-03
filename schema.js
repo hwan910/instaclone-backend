7b8a68957a2c2754b5a0167091d2574ed4651c43
sunghwanPark <william@chicment.com>
2023-05-04 00:15:19 +0900

    refactor: divide and conquer

--- schema.js
+++ schema.js
@@ -2,13 +2,12 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "apollo-server";
 
-const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`)
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const typeDefs = mergeTypeDefs(loadedTypes);
 
-const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`)
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 const resolvers = mergeResolvers(loadedResolvers);
 
-const schema = makeExecutableSchema({typeDefs,resolvers})
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-
-export default schema
+export default schema;
--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,5 @@
-import client from "../client";
+import client from "../../client";
 import bcrypt from "bcrypt";
-import Jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -43,30 +42,6 @@ export default {
       } catch (e) {
         return e;
       }
-    },
-    login: async (_, { userName, password }) => {
-      //find user with args.userName
-      const user = await client.user.findFirst({ where: { userName } });
-      if (!user) {
-        return {
-          ok: false,
-          error: "User not found.",
-        };
-      }
-      //check password with args.password
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: "Incorrect password.",
-        };
-      }
-      //issue a token and send it to the user
-      const token = Jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token,
-      };
     },
   },
 };
